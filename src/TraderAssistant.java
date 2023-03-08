6f2bbf63b401cd25f850f647ab399e85680595cc
Konstantin <medvedevblockchain@yandex.ru>
2023-03-09 01:43:19 +0530

    Пробуем делать комит в дев

--- src/TraderAssistant.java
+++ src/TraderAssistant.java
@@ -5,6 +5,7 @@ class TraderAssistant {
         this.username = username;
     }
 
+
     public void greetUser() {
         System.out.println("Здравствуйте, " + username + "! Добро пожаловать в Торгового помощника трейдера на криптовалютном обменнике!");
         System.out.println("Я могу помочь вам управлять портфелем, следить за статистикой и рассчитывать прибыль.");
